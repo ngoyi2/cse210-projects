4c3be884ada6dad3277a960e21e28a567d798377
NGOYI AUGUSTIN <augustinngoyi15@gmail.com>
2025-07-06 21:59:52 +0200

    Update Program.cs
    this is my page

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("Hello class This is the Exercise1 Project.");
+
+Console.Write("what is your first name? :");
+            String firstname = Console.ReadLine();
+
+            Console.Write("What is your last name? :");
+            string lastname = Console.ReadLine();
+
+            Console.Write($"your name is {firstname},{firstname} {lastname}.");
+
+
+
     }
 }
