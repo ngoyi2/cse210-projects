4d4d33072daf56bdb2a68516e80441bf397a761e
NGOYI AUGUSTIN <augustinngoyi15@gmail.com>
2025-07-07 01:32:53 +0200

    Update Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,49 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+    
+
+    string message = "congratulation you made it. see you next time";
+
+
+
+        Console.Write("what is your grade? :");
+        int result = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (result >= 90)
+        {
+            letter = "A";
+        }
+        else if (result >= 80)
+        {
+            letter = "B";
+        }
+        else if (result >= 70)
+        {
+            letter = "C";
+        }
+
+
+        else if (result >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+
+        if (result >= 70)
+        {
+            Console.Write($"your grade is {letter}, \n {message}");
+
+        }
+
+
     }
 }
