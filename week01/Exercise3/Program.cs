29439bf2395ce2185533b8924d44aacd89a01f8d
NGOYI AUGUSTIN <augustinngoyi15@gmail.com>
2025-07-12 09:58:30 -0800

    Refactor Exercise3 and Exercise4: Enhance guessing game logic and add sum, average, and max calculations

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+
+        Random generater = new Random();
+        int magicNumber = generater.Next(1, 101);
+
+        int guessnumber = -1;
+        while (guessnumber != magicNumber)
+        {
+            Console.Write(" what is you guessiung Number ? :");
+            guessnumber = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guessnumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (magicNumber < guessnumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You Guessed it congratulation");
+            }
+        }
     }
 }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Analyser";
+        job2._company = "Google";
+        job2._startYear = 2024;
+        job2._endYear = 2026;
+
+        Resume myResume = new Resume();
+        myResume._name = "Augustin ngoy";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
+
     }
 }
